edbb3b65a409e21bfc850109f2a041eee33c1c48
Erick <aspererick@gmail.com>
2026-03-16 09:48:42 -0300

    Adding a while loop to the main class on question 1, list 2. Allows for continuous execution of the menu.

--- Lista2/Q1/src/Main.java
+++ Lista2/Q1/src/Main.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Aniversario aniversario = new Aniversario();
-        System.out.println("Menu de opcoes: 1 adicionar amigo 2 atualizar amigo 3 remover amigo 4 listar amigos 5 sair  ");
         Scanner sc = new Scanner(System.in);
-        int opcao = sc.nextInt();
-        switch (opcao) {
-            case 1:
-                aniversario.adicionarAmigo();
-                break;
-            case 2:
-                aniversario.atualizarAmigo();
-                break;
-            case 3:
-                aniversario.removerAmigo();
-                break;
-            case 4:
-                aniversario.listarAmigos();
-                break;
-            case 5:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opcao invalida");
+        int opcao = 0;
+        while (opcao != 5) {
+            System.out.println("Menu de opcoes: 1 adicionar amigo 2 atualizar amigo 3 remover amigo 4 listar amigos 5 sair  ");
+            opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    aniversario.adicionarAmigo();
+                    break;
+                case 2:
+                    aniversario.atualizarAmigo();
+                    break;
+                case 3:
+                    aniversario.removerAmigo();
+                    break;
+                case 4:
+                    aniversario.listarAmigos();
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+            }
         }
         sc.close();
     }
